3650e96986f16c42443970a370df0e77221c5ef1
MutumaMutuma <lewismutuma1000@gmail.com>
2018-07-11 18:13:45 +0300

    first commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,39 @@
 alert("Welcome to Grocery Mania");
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
 $(document).ready(function() {
   $(".groceries").submit(function(event) {
     var item1 = $("#item1").val();
     var item2 = $("#item2").val();
     var item3 = $("#item3").val();
     var item4 = $("#item4").val();
-    var inPutItems = ["item1" + "item2" + "item3"+ "item4"];
-    console.log(inPutItems);
-    console.log(item1);
-    console.log(item2);
-    console.log(item3);
-    console.log(item4);
+    var shopping = [item1,item2,item3,item4];
+    console.log(shopping);
     event.preventDefault();
   });
+   $("").show();
 });
