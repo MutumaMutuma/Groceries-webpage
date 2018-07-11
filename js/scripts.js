644090b258e0b729e8abcf8701beb2ea6aa2eea5
MutumaMutuma <lewismutuma1000@gmail.com>
2018-07-11 08:02:38 +0300

    console.log all the items

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,11 @@ $(document).ready(function() {
     var item3 = $("#item3").val();
     var item4 = $("#item4").val();
     var inPutItems = ["item1" + "item2" + "item3"+ "item4"];
-  
+    console.log(inPutItems);
+    console.log(item1);
+    console.log(item2);
+    console.log(item3);
+    console.log(item4);
+    event.preventDefault();
   });
 });
