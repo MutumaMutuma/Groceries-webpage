1843c5734f68721cc4f42d320d8906362c2008dd
MutumaMutuma <lewismutuma1000@gmail.com>
2018-07-11 08:01:42 +0300

    Able to store items in an array

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,8 @@ $(document).ready(function() {
     var item1 = $("#item1").val();
     var item2 = $("#item2").val();
     var item3 = $("#item3").val();
-    
+    var item4 = $("#item4").val();
+    var inPutItems = ["item1" + "item2" + "item3"+ "item4"];
+  
   });
 });
