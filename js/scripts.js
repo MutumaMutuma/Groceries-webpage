79543124d73b126e92362afe28f179ff4d119e41
MutumaMutuma <lewismutuma1000@gmail.com>
2018-07-11 07:47:16 +0300

    Able to store the second item

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ alert("Welcome to Grocery Mania");
 $(document).ready(function() {
   $(".groceries").submit(function(event) {
     var item1 = $("#item1").val();
-
+    var item2 = $("#item2").val();
+    
   });
 });
