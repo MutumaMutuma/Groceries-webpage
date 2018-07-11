b9ead794517e8e7d0bbc46bf73a907965e70b5bb
MutumaMutuma <lewismutuma1000@gmail.com>
2018-07-11 08:00:54 +0300

    Able to store the third item

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
   $(".groceries").submit(function(event) {
     var item1 = $("#item1").val();
     var item2 = $("#item2").val();
+    var item3 = $("#item3").val();
     
   });
 });
